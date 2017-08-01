7cbc25e37e58f4e3261979c12a37f02ebeecf773
gabidavila <gabidavila@gmail.com>
2017-08-01 11:13:03 -0400

    added indexes

--- db/migrate/20170731154334_create_lines_table.rb
+++ db/migrate/20170731154334_create_lines_table.rb
@@ -3,8 +3,17 @@ class CreateLinesTable < ActiveRecord::Migration[5.0]
     create_table :lines do |t|
       t.integer :episodes_characters_location_id
       t.integer :number
+      t.integer :timestamp_in_ms
+      t.integer :character_id
+      t.integer :episode_id
+      t.integer :location_id
       t.text :character_line
       t.text :character_normalized_line
+      t.integer :word_count
     end
+
+    add_index :lines, :character_id
+    add_index :lines, :episode_id
+    add_index :lines, :location_id
   end
 end
--- db/migrate/20170731200046_create_episodes_characters_locations.rb
+++ db/migrate/20170731200046_create_episodes_characters_locations.rb
@@ -4,6 +4,10 @@ class CreateEpisodesCharactersLocations < ActiveRecord::Migration[5.0]
   		t.integer :episode_id
   		t.integer :character_id
   		t.integer :location_id
-  	end
+    end
+
+    add_index :episodes_characters_locations, :character_id
+    add_index :episodes_characters_locations, :episode_id
+    add_index :episodes_characters_locations, :location_id
   end
 end

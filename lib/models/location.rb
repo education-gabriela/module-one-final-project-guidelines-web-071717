e1c38b2fd060b073a6ad1fd174e009e5c3598751
gabidavila <gabidavila@gmail.com>
2017-08-01 14:11:07 -0400

    added relationships to location

--- lib/models/location.rb
+++ lib/models/location.rb
@@ -1,5 +1,8 @@
 class Location < ActiveRecord::Base
 	has_many :lines
+  has_many :appearances, through: :lines
+  has_many :episodes, through: :appearances
+  has_many :characters, through: :appearances
 
   extend Importable::ClassMethods
 end

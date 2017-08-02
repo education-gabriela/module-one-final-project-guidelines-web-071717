99dd62e8a1d350f12030efcdb2fd83cce0959892
Neil Steiner <npsteiner@gmail.com>
2017-08-02 10:33:29 -0400

    added support for 3 or more word commands

--- lib/cli/user_interaction.rb
+++ lib/cli/user_interaction.rb
@@ -9,7 +9,7 @@ class UserInteraction
   end
 
   def run
-    puts the_simpsons_logo
+    puts the_simpsons_logo.colorize(:yellow)
     show_commands
     input = get_user_input
   end
@@ -18,28 +18,31 @@ class UserInteraction
     {
       welcome: "WELCOME!",
       help: "sorry no help here",
-      invalid_input: "Sorry, that is not a valid command. Type 'help' to see a list of valid commands."
+      invalid_input: "Sorry, that is not a valid command.",
+      command_empty: "Command empty, please try again:"
     }
   end
 
   def the_simpsons_logo
     <<-SIMPSONS
+  _________________________________________________
+
   ___(_)_ __ ___  _ __  ___  ___  _ __  ___ 
  / __| | '_ ` _ \\| '_ \\/ __|/ _ \\| '_ \\/ __|
  \\__ \\ | | | | | | |_) \\__ \\ (_) | | | \\__ \\
  |___/_|_| |_| |_| .__/|___/\\___/|_| |_|___/
                  |_|
-
+  _________________________________________________
     SIMPSONS
   end
 
   def show_commands
     puts "Enter one of the following commands:\n\n"
     rows = []
-    puts "Usage: ".colorize(:yellow) + "<command> <subcommand> [<argument>, <value>]\n"
+    puts "Usage: ".colorize(:yellow) + "<command> <subcommand> [<argument> <value>]\n"
     puts "Sample:".colorize(:yellow)
-    puts "\tcharacter".colorize(:light_green) + " find_by name lisa"
-    puts "\tstats".colorize(:light_green) + " top_10_popular_episodes\n\n"
+    puts "\tcharacter".colorize(:light_green) + " find_by".colorize(:light_blue) + " name".colorize(:cyan) + " lisa".colorize(:light_red)
+    puts "\tstats".colorize(:light_green) + " top_10_popular_episodes\n\n".colorize(:light_blue)
     self.method_list.each do |method_name, method_list|
       rows << [method_name.to_s.colorize(:light_blue), method_list[:description]]
 
@@ -51,6 +54,7 @@ class UserInteraction
     end
     table = TTY::Table[*rows]
     puts table.to_s
+    puts "\n"
   end
 
   # welcome the user and then print help message containing valid commands
@@ -61,52 +65,64 @@ class UserInteraction
 
   # take in user input
   def get_user_input
+    print ">>  "
     input = gets.chomp
     parse_user_input(input)
   end
 
   def parse_user_input(input)
     possible_commands = input.split(" ")
-    # command1 will always be (super)method
-
+    # command1 will always be (super)method (sym except for help and exit)
     # iff exists.....
-    # command 2 will always be (sub)method
-    # command 3 will always be attribute searched
-    # command 4+ will always be value searched for
+    # command 2 will always be submethod (sym)
+    # command 3 will always be value searched (string)
+    
 
 
     # if command is empty
     if possible_commands.count == 0
-      puts "Command empty, please try again:".colorize(:red )
+      puts message_list[:command_empty].colorize(:red )
       get_user_input
 
-      # elsif command is exactly 'help'
+      # elsif command is exactly 'help' or 'exit'
     elsif possible_commands.count == 1
       if possible_commands[0] == "help"
         show_commands
         get_user_input
       elsif possible_commands[0] == "exit"
-#---------add exit method here
+        exit
+      else
+#------- invalid input        
       end
 
       # elsif command contains 2 words
     elsif possible_commands[0] && possible_commands[1]
       command1 = possible_commands[0].to_sym
       command2 = possible_commands[1].to_sym
+
       # params = possible_commands[2..-1]
       # binding.pry
-      if method_list[command1][:methods][command2]
-        class_name = method_list[command1][:methods][command2][:model]
-        method_name = method_list[command1][:methods][command2][:method]
 
-        class_name.to_s.singularize.capitalize.constantize.send(method_name)
-        # can add params later
-        # if command contains 3 words
+      # check to see that command1 (submethod) is valid before proceeding (avoid nil[] error)
+      if method_list[command1]
+        # check to see that command2 is a valid method
+        if  method_list[command1][:methods][command2]
+          # if no additional commands, process
+          if possible_commands.count == 2
+            class_name = method_list[command1][:methods][command2][:model]
+            method_name = method_list[command1][:methods][command2][:method]
 
-        # if command contains 4 words
+            call_query(class_name, method_name)
+          
+          # if command contains 3+ words
+          elsif possible_commands[2]
+            command3string  = possible_commands[2..-1].join(" ")
+            class_name = method_list[command1][:methods][command2][:model]
+            method_name = method_list[command1][:methods][command2][:method]
+            call_query_with_args(class_name, method_name, command3string)
+          end
 
-        # if longer command (i.e. searches for segments of lines)
-
+        end
 
       else
 #-------throw error and return to help
@@ -114,6 +130,9 @@ class UserInteraction
       end
       # binding.pry
     end
+
+    # return to beginning
+    get_user_input
 
   end
 
@@ -127,8 +146,14 @@ class UserInteraction
     end
   end
 
-  # make call to query method based on user input
-  def call_query
-
+  # given valid class_name and method_name, make the call
+  def call_query(class_name, method_name)
+    class_name.to_s.singularize.capitalize.constantize.send(method_name)
   end
+
+  def call_query_with_args(class_name, method_name, args)
+    puts class_name.to_s.singularize.capitalize.constantize.send(method_name, args)
+  end
+
+
 end

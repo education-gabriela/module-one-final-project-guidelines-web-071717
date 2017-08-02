9b5cf0ceaa50dc9330f97d8c79b34ad1154a98ad
gabidavila <gabidavila@gmail.com>
2017-08-02 10:28:45 -0400

    added methods to the yml

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,8 @@
 module Findable
   module ClassMethods
-    def name_search(**params)
-      class_values = self.where("normalized_name like :name", name: "%#{params[:name].downcase}%").pluck(:id, :name)
+    def name_search(param)
+      class_values = self.where("normalized_name like :name", name: "%#{param.downcase}%").pluck(:id, :name)
       keys = [:id, :name]
-      # binding.pry
       self.objectify(keys, class_values)
     end
   end

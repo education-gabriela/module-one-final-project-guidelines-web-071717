c5ed6905c9deba3e92c0fb7cc25425512e9fdab5
gabidavila <gabidavila@gmail.com>
2017-08-03 13:06:20 -0400

    converting limit to integer

--- lib/models/stat.rb
+++ lib/models/stat.rb
@@ -2,7 +2,7 @@ class Stat
   extend Formatable::ClassMethods
 
   def self.show_characters_stats(limit = 10)
-    stats = Character.joins(:lines).group("characters.id").limit(limit)
+    stats = Character.joins(:lines).group("characters.id").limit(limit.to_i)
               .pluck(:id, :name, "count(DISTINCT lines.location_id) as total_locations", "count(DISTINCT appearances.episode_id) as total_episodes", "count(*) AS total_lines")
     keys = [:id, :name, :total_locations, :total_episodes, :total_lines]
     hash = self.objectify(keys, stats)
@@ -10,7 +10,7 @@ class Stat
   end
 
   def self.show_episodes_stats(limit = 10)
-    stats = Episode.valid_episodes.joins(:lines).group(:id).limit(limit)
+    stats = Episode.valid_episodes.joins(:lines).group(:id).limit(limit.to_i)
               .pluck(:id, :title, "count(DISTINCT lines.location_id) as total_locations", "count(DISTINCT appearances.character_id) as total_characters", "count(*) AS total_lines")
     keys = [:id, :title, :total_locations, :total_characters, :total_lines]
     hash = self.objectify(keys, stats)
@@ -18,7 +18,7 @@ class Stat
   end
 
   def self.show_locations_stats(limit = 10)
-    stats = Line.joins(:location).joins(:appearance).group(:location_id).order("total_lines DESC").limit(limit)
+    stats = Line.joins(:location).joins(:appearance).group(:location_id).order("total_lines DESC").limit(limit.to_i)
               .pluck("locations.id", "locations.name", "count(DISTINCT appearances.character_id) as total_character_id", "count(DISTINCT appearances.episode_id) as total_episodes", "count(*) as total_lines")
     keys = [:id, :name, :total_characters, :total_episodes, :total_lines]
     hash = self.objectify(keys, stats)
